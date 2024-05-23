0c5a24aafdf580d94774ebe1b24d27301dd43d3f
Shaid Islam <shaid85@gmail.com>
2024-05-23 21:43:13 +0600

    All files added

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4a551b2b2a1ca364b78c');
---
+++ src/edit.js
@@ -0,0 +1,84 @@
+/**
+ * Retrieves the translation of text.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * React hook that is used to mark the block wrapper element.
+ * It provides all the necessary props like the class name.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ */
+import {
+	useBlockProps,
+	InspectorControls,
+} from '@wordpress/block-editor';
+
+import { PanelBody, TextControl, ToggleControl } from '@wordpress/components';
+
+/**
+ * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
+ * Those files can contain any CSS code that gets applied to the editor.
+ *
+ * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+export default function Edit({ attributes, setAttributes }) {
+	const { showStartingYear, startingYear } = attributes;
+
+	const currentYear = new Date().getFullYear().toString();
+
+	let displayDate;
+
+	if (showStartingYear && startingYear) {
+		displayDate = startingYear + '–' + currentYear;
+	} else {
+		displayDate = currentYear;
+	}
+
+	return (
+		<div {...useBlockProps()}>
+			<InspectorControls>
+				<PanelBody title={__('Settings', 'ews-example-block')}>
+					<ToggleControl
+						checked={!!showStartingYear}
+						label={__(
+							'Show starting year',
+							'copyright-date-block'
+						)}
+						onChange={() =>
+							setAttributes({
+								showStartingYear: !showStartingYear,
+							})
+						}
+					/>
+					{showStartingYear && (
+						<TextControl
+							label={__(
+								'Starting year',
+								'ews-example-block'
+							)}
+							value={startingYear || ''}
+							onChange={(value) =>
+								setAttributes({ startingYear: value })
+							}
+						/>
+					)}
+				</PanelBody>
+			</InspectorControls>
+			<p>© {displayDate}</p>
+		</div>
+
+	);
+}
